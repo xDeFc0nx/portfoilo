c714b699c21990acb9cb8289988585a41f836435
xDeFc0nx <nehartale8@gmail.com>
2024-02-11 18:18:06 +0100

    feat: hero finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Hero } from "./components/UI/Hero";
 
 export default function Home() {
   return (
     <div>
-      <div></div>
+      {/* <Hero /> */}
+      <div>
+        <Hero />
+      </div>
     </div>
   );
 }
