db7be2e95c1d03dc37a2ae825995797b2306e7f1
xDeFc0nx <nehartale8@gmail.com>
2024-12-14 00:08:22 +0100

    fix: added responsivnes and bug fixes

--- client/src/Components/hero/index.tsx
+++ client/src/Components/hero/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
@@ -22,31 +20,27 @@ function index() {
 
   return (
     <div>
-      <div className="bg-[#D225E1] -z-1 w-[20rem] h-[30rem] rounded-full blur-[10rem] fixed  left-[30rem] -translate-y-1/2" />
-
-      <div className="bg-[#2565E1] -z-1 w-[20rem] h-[30rem] rounded-full blur-[10rem] fixed  left-[15rem] -translate-y-1/2" />
-      <div className="flex">
-        <div className="flex flex-col">
-          <span className="sm:text-sm md:text-[30px] lg:text-[50px] text-white z-auto flex text-pretty pt-[15rem] ml-24 ">
-            Transforming ideas into reality <br />
-            where vision meets precision
+      <div className="bg-[#5B5CB4] -z-1 w-[45rem] h-[45rem] rounded-full blur-[10rem] fixed left-[30rem] -translate-y-1/2" />
+      <div className="bg-[#D225E1] -z-1 w-[20rem] h-[20rem] rounded-full blur-[10rem] fixed left-[10rem] -translate-y-1/2" />
+      <div className="bg-[#2565E1] -z-1 w-[20rem] h-[20rem] rounded-full blur-[10rem] fixed left-[15rem] -translate-y-1/2" />
+      <div className="flex flex-col justify-center max-w-2xl m-auto md:max-w-4xl xl:max-w-[1280px] xl:grid xl:grid-cols-3 gap-16 xl:gap-24 min-h-screen xl:h-screen items-center px-8">
+        <div className="flex flex-col xl:col-span-2">
+          <span className="text-[48px] leading-[54px] xl:text-[54px] xl:leading-[64px] font-semibold text-center xl:text-left text-white">
+            Transforming ideas into reality: where vision meets precision
           </span>
-          <span className="text-[32px] pt-5 text-white/45  z-auto flex ml-24 ">
-            Nehar Tale, Your hero Software Engineer, <br />
-            leading us to victory in the war against unmaintainable code.
+          <span className="text-[28px] leading-[35px] xl:text-[32px] xl:leading-[48px] pt-6 text-gray-400 text-center xl:text-left">
+            Nehar Tale, your hero programmer and web developer, leading us to
+            victory in the war against unmaintainable code.
           </span>
         </div>
-
-        <div className="ml-24 pt-52 ">
-          <div className="rounded-xl w-[40rem] overflow-hidden bg-[#1a232e]/10 backdrop-blur-lg border border-white/30  p-4 text-md ">
-            <div className=" flex justify-between pb-6 pt-3  ">
-              <div className=" flex space-x-2">
-                <div className="w-4 h-4 rounded-full border-2 border-white	"></div>
-                <div className="w-4 h-4 rounded-full border-2 border-white	"></div>
-                <div className="w-4 h-4 rounded-full border-2 border-white	"></div>
-              </div>
-            </div>
-            <div className="w-full h-[1] border-2  border-white/30 	"></div>
+        <div className="w-full bg-white/10 border border-white/10 rounded-2xl p-8 flex flex-col gap-6">
+          <div className="flex gap-4">
+            <div className="w-4 h-4 rounded-full bg-red-500"></div>
+            <div className="w-4 h-4 rounded-full bg-yellow-500"></div>
+            <div className="w-4 h-4 rounded-full bg-green-500"></div>
+          </div>
+          <div className="bg-[#CFECFD] -z-1 w-[20rem] h-[20rem] rounded-full blur-[10rem] fixed left-[10rem] -translate-y-1/2" />
+          <div className="flex flex-col gap-2">
             <SyntaxHighlighter
               language="javascript"
               style={atomOneDark}
@@ -56,8 +50,6 @@ function index() {
             </SyntaxHighlighter>
           </div>
         </div>
-        <div className="bg-[#2565E1] -z-1 w-[20rem] h-[20rem] rounded-full blur-[10rem]  fixed mt-[20rem] ml-[120rem]" />
-        <div className="bg-[#CFECFD] -z-1 w-[20rem] h-[20rem] rounded-full blur-[10rem]  fixed mt-[35rem] ml-[120rem]" />
       </div>
     </div>
   );
