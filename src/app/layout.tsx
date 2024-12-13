64263132caa4d5e18496fca93c822755fb35f32f
Arbion Halili <99731180+ToxicPine@users.noreply.github.com>
2024-12-13 22:28:50 +0000

    Tweaks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "./components/UI/Navbar";
+import { Navbar } from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="absolute z-[-1]">
+          <div className="bg-[#5B5CB4] -z-1 w-[45rem] h-[45rem] rounded-full blur-[10rem] -mt-96 -ml-14" />
+          <div className="bg-[#D225E1] -z-1 w-[20rem] h-[20rem] rounded-full blur-[10rem] -mt-10 ml-[30rem]" />
+          <div className="bg-[#2565E1] -z-1 w-[20rem] h-[20rem] rounded-full blur-[10rem] -mt-10 ml-[40rem]" />
+          <div className="bg-[#CFECFD] -z-1 w-[20rem] h-[20rem] rounded-full blur-[10rem] -mt-10 ml-[50rem]" />
+        </div>
         <Navbar />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 import React from "react";
-import { Hero } from "./components/UI/Hero";
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
-  return (
-    <div>
-      {/* <Hero /> */}
-      <div>
-        <Hero />
-      </div>
-    </div>
-  );
+  return <Hero />;
 }
