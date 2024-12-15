c6519afa7b81f6dd97e0e5d80194d4cf6bc392f3
xDeFc0nx <nehartale8@gmail.com>
2024-12-15 04:17:25 +0100

    feat: added about me

--- client/src/Components/aboutme/index.tsx
+++ client/src/Components/aboutme/index.tsx
@@ -1,5 +1,30 @@
-function aboutme() {
-  return <div>aboutme</div>;
+import pfp from "../../assets/profile.jpeg";
+function index() {
+  return (
+    <>
+      <div className="flex flex-col  max-w-2xl m-auto md:max-w-4xl xl:max-w-96 xl:grid  min-h-screen xl:h-screen ">
+        <div className="flex flex-col xl:col-span-1 items-center pt-24 lx:pt-0   ">
+          <span className="text-[30px] leading-[30px] xl:text-[40px] xl:leading-[60px] pb-5  text-center xl:text-left  text-white">
+            About me
+          </span>
+          <img src={pfp} alt="" className="rounded-full h-32 w-32 " />
+          <span className="text-[18px]  leading-[25px] xl:text-[22px] xl:leading-[38px] pt-6 text-gray-400 text-center ">
+            You know that spark of inspiration, the "aha!" moment where
+            everything clicks—but then you realize it needs a roadmap, a system,
+            and a spark of innovation to bring it to life? That’s where I come
+            in. I’m a software engineer who thrives in the space between
+            imagination and execution. Whether it’s coding up a seamless user
+            interface or designing the backend logic that powers it all, I bring
+            structure to creativity and consistency to chaos. <br />
+            <span className="font-bold">
+              Let’s turn your idea into something real.
+            </span>
+          </span>
+        </div>
+        <div className="bg-[#5B5CB4] -z-1 xl:w-96  xl:h-96 md:w-52 md:h-52 w-32 h-32 rounded-full blur-[4rem] xl:blur-[12rem] absolute  -translate-x-3/4" />
+      </div>
+    </>
+  );
 }
 
-export default aboutme;
+export default index;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -20,5 +20,5 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
     <ToastContainer />
-  </StrictMode>,
+  </StrictMode>
 );
