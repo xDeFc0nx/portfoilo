fa48cf612717d49f760d7e247a331b9b2849d60b
xDeFc0nx <nehartale8@gmail.com>
2024-12-19 16:14:57 +0100

    fix: re worked main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,38 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Login from "./pages/login";
-import Project from "./pages/Project";
-import Dashboard from "./pages/admin/dashboard.tsx";
-import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/project/:id",
-    element: <Project />,
-  },
-  {
-    path: "*",
-    element: <div>Page Not Found</div>,
-  },
-  {
-    path: "/admin/dashboard",
-    element: <Dashboard />,
-  },
-]);
+import { ToastContainer } from "react-toastify";
+import "@/index.css";
+import App from "@/App.tsx";
+import Login from "@/pages/login";
+import Project from "@/pages/Project";
+import ProjectsPage from "@/pages/admin/projects";
+import AddProject from "@/pages/admin/projects/addProject";
+import Dashboard from "@/pages/admin/dashboard.tsx";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/project/:id" element={<Project />} />
+
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/projects" element={<ProjectsPage />} />
+        <Route path="/admin/projects/add" element={<AddProject />} />
+      </Routes>
+    </BrowserRouter>
     <ToastContainer />
   </StrictMode>
 );
