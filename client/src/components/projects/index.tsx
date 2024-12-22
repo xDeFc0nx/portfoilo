3ff55529ab7688dd829f0362953f73a96756e418
xDeFc0nx <nehartale8@gmail.com>
2024-12-22 11:56:10 +0100

    feat: pagination

--- client/src/components/projects/index.tsx
+++ client/src/components/projects/index.tsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
 import Card from "./card";
-
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 interface Project {
   ID: number;
   Title: string;
@@ -10,29 +17,47 @@ interface Project {
 
 function Index() {
   const [projects, setProjects] = useState<Project[]>([]);
+  const rowsPerPage = 6;
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(rowsPerPage);
+
+  const fetchProjects = async () => {
+    const response = await fetch("http://127.0.0.1:3000/api/getprojects");
+    const data = await response.json();
+    setProjects(data.Projects);
+  };
   useEffect(() => {
-    const fetchProjects = async () => {
-      const response = await fetch("http://127.0.0.1:3000/api/getprojects");
-      const data = await response.json();
-      setProjects(data.Projects);
-    };
     fetchProjects();
   }, []);
 
   if (!projects || projects.length === 0)
     return <div className="text-white">Loading...</div>;
 
+  const handlePrevious = () => {
+    if (startIndex > 0) {
+      setStartIndex(startIndex - rowsPerPage);
+      setEndIndex(endIndex - rowsPerPage);
+    }
+  };
+
+  const handleNext = () => {
+    if (endIndex < projects.length) {
+      setStartIndex(startIndex + rowsPerPage);
+      setEndIndex(endIndex + rowsPerPage);
+    }
+  };
   return (
     <>
-      <div className=" max-w-2xl m-auto md:max-w-4xl snap-start scroll-smooth xl:max-w-[1280px] gap-16 xl:gap-1 min-h-screen xl:h-screen items-center px  ">
-        <div className="flex flex-col xl:col-span-1 items-center pt-24 lx:pt-0   ">
-          <span className="text-[30px] leading-[30px] xl:text-[40px] xl:leading-[60px] pb-5  text-center xl:text-left  text-white">
+      <div className="max-w-2xl m-auto md:max-w-4xl snap-start scroll-smooth xl:max-w-[1280px] gap-16 xl:gap-1 min-h-screen xl:h-screen items-center px">
+        <div className="flex flex-col xl:col-span-1 items-center pt-24 lx:pt-0">
+          <span className="text-[30px] leading-[30px] xl:text-[40px] xl:leading-[60px] pb-5 text-center xl:text-left text-white">
             Projects
           </span>
-          <div className="grid md:grid-cols-3 gap-16 ">
-            <div className="contents xl:col-span-3 items-center  xl:pt-0">
-              {projects.map((project) => (
+          <div className="grid md:grid-cols-3 gap-16">
+            <div className="contents xl:col-span-3 items-center xl:pt-0">
+              {projects.slice(startIndex, endIndex).map((project) => (
                 <Card
+                  key={project.ID}
                   ID={project.ID}
                   Logo={project.Logo}
                   Title={project.Title}
@@ -41,6 +66,37 @@ function Index() {
               ))}
             </div>
           </div>
+          <Pagination className="pt-10">
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  className={
+                    startIndex === 0
+                      ? "pointer-events-none opacity-50"
+                      : undefined
+                  }
+                  href="#"
+                  onClick={handlePrevious}
+                />
+              </PaginationItem>
+
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+
+              <PaginationItem>
+                <PaginationNext
+                  className={
+                    endIndex >= projects.length
+                      ? "pointer-events-none opacity-50"
+                      : undefined
+                  }
+                  href="#"
+                  onClick={handleNext}
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       </div>
     </>
