5e5c126739be2a8c53331fbcb807eece154915d5
Nehar Tale <nehartale8@gmail.com>
2025-01-12 18:06:56 +0100

    creating admin

--- handlers/auth.go
+++ handlers/auth.go
@@ -6,19 +6,11 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/joho/godotenv"
-	"github.com/xDeFc0nx/logger-go-pkg"
 
 	"github.com/xDeFc0nx/portofoilo/models"
 )
 
 func Create_JWT_Token(user models.User) (string, int64, error) {
-	err := godotenv.Load(".env")
-	if err != nil {
-		logger.Error("Error loading.env file")
-		os.Exit(1)
-	}
-
 	SECRET_KEY := os.Getenv("SECRET_KEY")
 	exp := time.Now().Add(time.Minute * 30).Unix()
 	token := jwt.New(jwt.SigningMethodHS256)
@@ -35,9 +27,13 @@ func Create_JWT_Token(user models.User) (string, int64, error) {
 func Check_Auth_func(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt-token")
 
-	token, err := jwt.ParseWithClaims(cookie, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte(os.Getenv("SECRET_KEY")), nil
-	})
+	token, err := jwt.ParseWithClaims(
+		cookie,
+		&jwt.MapClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("SECRET_KEY")), nil
+		},
+	)
 	if err != nil || !token.Valid {
 		return c.Status(401).JSON(fiber.Map{"error": "Unauthorized"})
 	}
